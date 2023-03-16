f23a19513fdb9f4e441256ff1829ffb521ac655e
Neil_Mwaki <muthamineil@gmail.com>
2023-03-16 12:13:30 -0700

    output using the printf function

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/*
+ * main - The function prints the text using printf std function
+ * Description: the printf function prints the word in double quotes
+ * return: zero which is a requirement
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
