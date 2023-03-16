b3c76cf2c1831563c2391306a93e7d376bbf6df4
Neil_Mwaki <muthamineil@gmail.com>
2023-03-16 13:12:08 -0700

    corretct output iteration

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 /**
  * main - prints the size of int, char and float type in bytes
- * return: zero to satisfy the main return type
+ * Return: zero to satisfy the main return type
  */
 int main(void)
 {
@@ -9,10 +9,10 @@ int main(void)
 	float floatType;
 	char charType;
 
-	printf("Size of a char:%zu byte(s)\n", sizeof(charType));
-	printf("Size of an int:%zu byte(s)\n", sizeof(intType));
-	printf("Size of a long int:%zu byte(s)\n", sizeof((long) intType));
-	printf("Size of a long long int:%zu byte(s)\n", sizeof((long long) intType));
-	printf("Size of a float:%zu byte(s)\n", sizeof(floatType));
+	printf("Size of a char: %zu byte(s)\n", sizeof(charType));
+	printf("Size of an int: %zu byte(s)\n", sizeof(intType));
+	printf("Size of a long int: %zu byte(s)\n", sizeof((long) intType));
+	printf("Size of a long long int: %zu byte(s)\n", sizeof((long long) intType));
+	printf("Size of a float: %zu byte(s)\n", sizeof(floatType));
 	return (0);
 }
