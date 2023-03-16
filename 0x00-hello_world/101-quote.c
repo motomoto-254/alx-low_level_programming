3ced19f08a3eb8f44181c846454c3f43e9007d3a
Neil_Mwaki <muthamineil@gmail.com>
2023-03-16 14:13:51 -0700

    print text

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - prints out the script using puts
+ * Return: 1 per requirements of return value
+ */
+int main(void)
+{
+	puts("and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+	return (1);
+}
