e98c4081f3f09173d948e81e1bbbc4e9108b52f1
Neil_Mwaki <muthamineil@gmail.com>
2023-03-17 17:48:07 -0700

    display last digit on int variable with if statements

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,33 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+
+/**
+ * main - checks the last stored variable which is checked under if
+ * statements expressions and print relevant strings
+ * Return: zero to satisfy the int type of the main function
+ */
+
+int main(void)
+{
+	int n;
+	int result;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	result = (n % 10)
+	if (result > 5)
+	{
+		printf("Last digit of %d is %d and is greater than 5", n, result);
+	}
+	else if (result == 0)
+	{
+		printf("Last digit of %d is %d and is 0", n, result);
+	}
+	else
+	{
+		printf("Last digit of %d is %d and is less than 6 and not 0", n, result);
+	}
+
+	return (0);
+}
