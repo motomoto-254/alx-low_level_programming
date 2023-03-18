d9b0f616e5edce697be3dd965a5ced1c259b24d3
Neil_Mwaki <muthamineil@gmail.com>
2023-03-17 18:18:57 -0700

    print lower case alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - prints the letters a to z using a if loop
+ * Return: zero to satisfy the int main type
+ */
+
+int main(void)
+{
+	int letter;
+	char alpha[25] = [a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t, u, v, w, x, y, z];
+
+	letter = 0;
+	if (letter == 0)
+	{
+		putchar(alpha[letter]);
+		++letter;
+	}
+	else
+	{
+		putchar(alpha[letter]);
+		++letter;
+	}
+	return (0);
+}
