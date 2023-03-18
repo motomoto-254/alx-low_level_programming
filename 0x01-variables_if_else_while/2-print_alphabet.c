ed528554e50a0228a38efc0460d8a58429a821da
Neil_Mwaki <muthamineil@gmail.com>
2023-03-17 18:27:13 -0700

    print alphabet using for loop

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,26 +1,14 @@
 #include <stdio.h>
 
 /**
- * main - prints the letters a to z using a if loop
+ * main - prints the letters a to z using a for loop
  * Return: zero to satisfy the int main type
  */
 
 int main(void)
 {
-	int letter;
-	char alpha[25];
-
-	alpha = [a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t, u, v, w, x, y, z];
-	letter = 0;
-	if (letter == 0)
-	{
-		putchar(alpha[letter]);
-		++letter;
-	}
-	else
-	{
-		putchar(alpha[letter]);
-		++letter;
-	}
+	for (i = 'a'; i <= 'z'; i++)
+		putchar(i);
+	putchar('\n');
 	return (0);
 }
