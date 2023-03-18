d7a23763c2f35872d91614a51c4a9a8b0b9c0b96
Neil_Mwaki <muthamineil@gmail.com>
2023-03-17 18:22:15 -0700

    print alphabet compliation error checker

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -8,8 +8,9 @@
 int main(void)
 {
 	int letter;
-	char alpha[25] = [a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t, u, v, w, x, y, z];
+	char alpha[25];
 
+	alpha = [a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t, u, v, w, x, y, z];
 	letter = 0;
 	if (letter == 0)
 	{
