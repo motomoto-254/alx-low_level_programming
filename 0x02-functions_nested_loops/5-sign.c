aa9f4c9fdba2d40b970af03827dabe0a1b5db0e5
Neil_Mwaki <muthamineil@gmail.com>
2023-03-21 14:11:38 -0700

    5-sign code

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,28 +1,7 @@
 #include "main.h"
 
 /**
- * main - runs the print_sign function
- * Return: zero
- */
-
-int main(void)
-{
-	int x;
-
-	x = print_sign(9);
-	_putchar(x + '0');
-	_putchar('\n');
-	x = print_sign(-2);
-	_putchar(x + '0');
-	_putchar('\n');
-	x = print_sign(0);
-	_putchar(x + '0');
-	_putchar('\n');
-	return (0);
-}
-
-/**
- * print_sign - checks number if zero, less than or greater than zero
+*  print_sign - checks number if zero, less than or greater than zero
  * @n: input variable for checks
  *
  * Return: 1 if greater than 0, 0 if 0, -1 if < 0
