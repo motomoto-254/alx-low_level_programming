efc386c3cefd45ea2c33c5d0f201763a93e16a38
Neil_Mwaki <muthamineil@gmail.com>
2023-03-21 13:52:39 -0700

    2-print_alphabet_x10, main entry error correction

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,17 +1,6 @@
 #include "main.h"
 
 /**
- * main - print alphabet 10 times
- * Return: zero
- */
-
-int main(void)
-{
-	print_alphabet_x10();
-	return (0);
-}
-
-/**
  *  print_alphabet_x10 - use for loops to print the alphabet
  */
 
