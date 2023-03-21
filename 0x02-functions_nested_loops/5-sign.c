4a7df697e81d060267986b6cf689feb49a3e58fe
Neil_Mwaki <muthamineil@gmail.com>
2023-03-21 14:26:21 -0700

    5-sign code correction

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -9,20 +9,23 @@
 
 int print_sign(int n)
 {
+	int val;
+
 	if (n > 0)
 	{
-		return (1);
+		val = 1;
 		_putchar('+');
 	}
 	else if (n == 0)
 	{
-		return (0);
-		_putchar(0 + 0);
+		val = 0;
+		_putchar(0 + '0');
 	}
-	else if (n < 0)
+	else
 	{
-		return (-1);
+		val = -1;
 		_putchar('-');
 	}
+	return (val);
 }
 
