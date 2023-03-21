5b87c7f24b447a7d0d87d89edadd5cbc8243fe86
Neil_Mwaki <muthamineil@gmail.com>
2023-03-21 13:56:53 -0700

    3-islower main entry correction

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,27 +1,6 @@
 #include "main.h"
 
 /**
- * main - runs checks on letter and prints results
- * Return: zero
- */
-
-int main(void)
-{
-	int r;
-
-	r = _islower('M');
-	_putchar(r + '0');
-	r = _islower('o');
-	_putchar(r + '0');
-	r = _islower('T');
-	_putchar(r + '0');
-	r = _islower('o');
-	_putchar(r + '0');
-	_putchar('\n');
-	return (0);
-}
-
-/**
  * _islower - checks for lowercase
  * @c: character to check islower
  *
