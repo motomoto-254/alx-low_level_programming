a2fee15c28c3f27d11572a7c41388e20d1c382db
Neil_Mwaki <muthamineil@gmail.com>
2023-03-23 11:12:09 -0700

    print numbers

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_numbers - functions to print numbers from 0 to 10
+ * 
+ * Return: none/void
+ */
+
+void print_numbers(void)
+{
+	int x;
+
+	for (x = 0; x <= 10; x++)
+	{
+		_putchar(x + '0');
+	}
+	_putchar('\n');
+}
+
