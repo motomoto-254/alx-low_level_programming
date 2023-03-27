f013a55829afa731e83a0592955a57804070c910
Neil_Mwaki <muthamineil@gmail.com>
2023-03-27 14:03:59 -0700

    print reverse array

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -10,13 +10,13 @@ void rev_string(char *s)
 	int n, l;
 
 	l = 0;
-	while (s[l] != '\0')
+	while (*(s + l) != '\0')
 	{
 		l++;
 	}
 	for (n = l - 1; n >= 0; n--)
 	{
-		_putchar(s[n]);
+		_putchar(*(s + n));
 	}
 	_putchar('\n');
 }
