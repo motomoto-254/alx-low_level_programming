f893aad0b62e96bf0bb3a4ae2bb1ec012722dd0e
Neil_Mwaki <muthamineil@gmail.com>
2023-03-27 12:16:15 -0700

    print a string

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _puts - print the string input
+ * @str: pointer to input variable
+ *
+ * Return: void
+ */
+
+void _puts(char *str)
+{
+	while (*str != '\0')
+	{
+		_putchar(*str);
+		str++;
+	}
+}
+
