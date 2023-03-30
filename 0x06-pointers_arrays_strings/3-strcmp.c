e6aa10d3bc0b2bad7843b13cf11d7c6384408a1f
Neil_Mwaki <muthamineil@gmail.com>
2023-03-30 08:50:44 -0700

    update string compare

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -20,6 +20,10 @@ int _strcmp(char *s1, char *s2)
 	}
 	if (s1[i] == '\0' && s2[i] == '\0')
 		return (0);
+	else if(s1[i] == '\0')
+		return (-s2[i]);
+	else
+		return (s1[i]);
 
 	return (ret);
 }
