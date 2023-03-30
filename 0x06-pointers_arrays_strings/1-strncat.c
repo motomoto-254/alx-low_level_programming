5b21f83330d66fac14c25cb142479aea422f387d
Neil_Mwaki <muthamineil@gmail.com>
2023-03-30 04:37:44 -0700

    concatenate two string with limit of n byte correction

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -15,11 +15,11 @@ char *_strncat(char *dest, char *src, int n)
 	x = 0;
 	while (dest[x] != '\0')
 		x++;
-	for (i = 0; src[i] != '\0'; i++)
+	for (i = 0; i < n && src[i] != '\0'; i++)
 	{
-		if (i <= n)
-			_putchar(dest[x + i]);
+		(dest[x + i]) = src[i];
 	}
+	dest[x + i] = '\0';
 	return (dest);
 }
 
