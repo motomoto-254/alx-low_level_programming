98afb1cb7a37d705c0dd66387f3948a40d0b1e88
Neil_Mwaki <muthamineil@gmail.com>
2023-03-30 08:18:52 -0700

    string compare correction

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -11,16 +11,16 @@ int _strcmp(char *s1, char *s2)
 {
 	int i, ret;
 
-	for (i = 0; s1[i] != '\0'; i++)
+	for (i = 0; s1[i] != '\0' && s2[i] != '\0'; i++)
 	{
 		if (s1[i] < s2[i])
-			ret = (s1[i] - s2[i]);
-		else if (s1[i] == s2[i])
-			ret = (s1[i] - s2[i]);
-		else
-			ret = (s1[i] - s2[i]);
-		i++;
+			ret = (s2[i] - s1[i]);
+		else if (s1[i] > s2[i])
+			ret = (s2[i] - s1[i]);
 	}
+	if (s1[i] == '\0' && s2[i] == '\0')
+		return (0);
+
 	return (ret);
 }
 
