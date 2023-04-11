ec849357611a5b01af16f8babca774ca39dea80b
Neil_Mwaki <muthamineil@gmail.com>
2023-04-10 18:15:23 -0700

    requirements correction

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -4,13 +4,13 @@
 
 /**
  * main - program that prints its name
- * argc: count to input arguments
- * argv: pointer to string input
+ * @argc: count to input arguments
+ * @argv: pointer to string input
  *
  * Return: int
  */
 
-int main(int argc, char *argv[])
+int main(__attribute__((unused)) int argc, char *argv[])
 {
 	int i;
 
