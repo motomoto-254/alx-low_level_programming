89c55b0a0744af66a946147d3d757023eb599943
Neil_Mwaki <muthamineil@gmail.com>
2023-04-11 16:04:50 -0700

    Null handling error correction

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -16,12 +16,13 @@ char *create_array(unsigned int size, char c)
 	array = malloc(sizeof(char) * size);
 	if (array == NULL)
 		return (NULL);
-
+	else if (size == 0)
+		return (NULL);
 	for (i = 0; i < size; i++)
 	{
 		array[i] = c;
 	}
 	return (array);
-	free (array);
+	free(array);
 }
 
