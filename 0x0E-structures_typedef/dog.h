b0351d8a6f35f8e656a3800eb34320c001673497
Neil_Mwaki <muthamineil@gmail.com>
2023-04-18 10:27:16 -0700

    new type dog_t as a new name the type struct dog

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -10,12 +10,12 @@
  * Description: basic traits assigned to a particular dog
  */
 
-struct dog
+type_def struct
 {
 	char *name;
 	float age;
 	char *owner;
-};
+}dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
