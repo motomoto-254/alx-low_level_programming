5e7b994455220a3c2d35e5b803f231c816ee0691
Neil_Mwaki <muthamineil@gmail.com>
2023-04-18 11:18:50 -0700

    dog header correction

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -10,13 +10,13 @@
  * Description: basic traits assigned to a particular dog
  */
 
-type_def struct
+struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-}dog_t;
-
+};
+typedef struct dog dog_t;
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 
