2716a19f6208494d9d229ef09aff2e5260b182b7
Neil_Mwaki <muthamineil@gmail.com>
2023-04-18 14:06:30 -0700

    print dog struct correction

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -10,8 +10,6 @@
  */
 void print_dog(struct dog *d)
 {
-	float i = -1.0;
-
 	if (d == NULL)
 		return;
 
@@ -19,10 +17,7 @@ void print_dog(struct dog *d)
 		printf("Name: %s\n", d->name);
 	else
 		printf("(nil)\n");
-	if ((d->age) != i)
-		printf("Age: %f\n", d->age);
-	else
-		printf("(nil)\n");
+	printf("Age: %f\n", d->age);
 	if ((d->owner) != NULL)
 		printf("Owner: %s\n", d->owner);
 	else
---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,20 @@
+#include "dog.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * new_dog - creates a new dog
+ * @name: attribute element
+ * @age: attribute element
+ * @owner: attribute element
+ *
+ * Return: null if function fails
+ */
+
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	char new_name, new_owner;
+
+
+}
+
