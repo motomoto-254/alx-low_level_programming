b4557e876b1f628d7abb1e17e22489bcccd2f2e6
Neil_Mwaki <muthamineil@gmail.com>
2023-04-18 17:23:44 -0700

    create a new dog

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,20 +1,70 @@
 #include "dog.h"
-#include <stdio.h>
 #include <stdlib.h>
+#include <stdio.h>
 
 /**
- * new_dog - creates a new dog
+ * strcount - count number of char in string
+ * @s: string pointer
+ *
+ * Return: int
+ */
+int strcount(char *s);
+
+/**
+ * new_dog - function creates a new dog
  * @name: attribute element
  * @age: attribute element
  * @owner: attribute element
  *
- * Return: null if function fails
+ * Return: dog_t pointer
  */
 
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	char new_name, new_owner;
+	int i;
+	dog_t *dog_var;
+	char *new_name, *new_owner;
 
+	dog_var = malloc(sizeof(dog_t));
+	if (dog_t == NULL)
+		return (NULL);
 
+	new_name = malloc(sizeof(char) * (strcount(name) + 1));
+	if (new_name == NULL)
+		return (NULL);
+	i = 0;
+	while (name[i] != '\0')
+	{
+		new_name[i] = name[i];
+		i++;
+	}
+	new_name[i] = '\0';
+	dog_var->name = new_name;
+
+	dog_var->age = age;
+
+	new_owner = malloc(sizeof(char) * (strcount(owner) + 1));
+	if (new_owner == NULL)
+		return (NULL);
+	i = 0;
+	while (owner[i] != '\0')
+	{
+		new_owner[i] = owner[i];
+		i++;
+	}
+	new_owner[i] = '\0';
+	dog_var->owner = new_owner;
+
+	return (dog_var);
+}
+
+int strcount(char *s)
+{
+	int i;
+
+	i = 0;
+	while (s[i] != '\0')
+		i++;
+	return (i);
 }
 
